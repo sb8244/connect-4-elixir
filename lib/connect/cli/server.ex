5c27437b36454bf5403413e10184360ab7939abc
Stephen Bussey <steve.bussey@salesloft.com>
2016-08-21 15:33:42 -0400

    play against a (bad) AI

--- lib/connect/cli/server.ex
+++ lib/connect/cli/server.ex
@@ -1,15 +1,22 @@
 defmodule Connect.Cli.Server do
   def start do
     game_choice = IO.gets "\nDo you want to play Connect 4 (1) or Tic Tac Toe (2)\n"
+    use_ai = case IO.gets "\nDo you want to play against an AI (y or n)\n" do
+      "y\n" -> true
+      _ -> false
+    end
+
     case game_choice do
       "1\n" ->
         {:ok, game} = Connect.Game.Server.start_link(nil, %{rows: 6, columns: 7, win_size: 4})
         IO.puts "Starting Connect 4!\n\n"
-        play_game(:connect_4, game, 0)
+
+        ai_module = if use_ai, do: Connect.Game.Ai.Connect4, else: nil
+        play_game(:connect_4, game, 0, ai_module)
       "2\n" ->
         {:ok, game} = Connect.Game.Server.start_link(nil, %{rows: 3, columns: 3, win_size: 3})
         IO.puts "Starting Tic Tac Toe!\n\n"
-        play_game(:tto, game, 0)
+        play_game(:tto, game, 0, nil)
       _ ->
         IO.puts "-_-"
         exit(:shutdown)
@@ -18,10 +25,10 @@ defmodule Connect.Cli.Server do
     IO.gets "Game over! Enter to start a new game....."
   end
 
-  defp play_game(_type, _game, turn) when turn == nil do
+  defp play_game(_type, _game, turn, _ai_module) when turn == nil do
   end
 
-  defp play_game(:tto, game, turn) do
+  defp play_game(:tto, game, turn, ai_module) do
     current_player = rem(turn, 2) + 1
     IO.puts "Player #{current_player}'s turn."
     print_board(game)
@@ -43,15 +50,25 @@ defmodule Connect.Cli.Server do
         get_next_turn_from_move(game, move_result, turn)
     end
 
-    play_game(:tto, game, next_turn)
+    play_game(:tto, game, next_turn, ai_module)
   end
 
-  defp play_game(:connect_4, game, turn) do
+  defp play_game(:connect_4, game, turn, ai_module) do
     current_player = rem(turn, 2) + 1
-    IO.puts "Player #{current_player}'s turn. Which column (1-7) do you want to place?"
-    print_board(game)
-    placement = IO.gets ""
-    IO.puts "\n\n"
+    IO.puts "Player #{current_player}'s turn.\n"
+
+    placement = cond do
+      current_player == 2 && ai_module ->
+        game_state = Connect.Game.Server.get_state(game)
+        {:ok, placement} = ai_module.get_placement(game_state, current_player)
+        Integer.to_string(placement+1)
+      true ->
+        IO.puts "Which column (1-7) do you want to place?"
+        print_board(game)
+        placement = IO.gets ""
+        IO.puts "\n\n"
+        placement
+    end
 
     next_turn = case Integer.parse(placement) do
       :error ->
@@ -62,7 +79,7 @@ defmodule Connect.Cli.Server do
         get_next_turn_from_move(game, move_result, turn)
     end
 
-    play_game(:connect_4, game, next_turn)
+    play_game(:connect_4, game, next_turn, ai_module)
   end
 
   defp get_next_turn_from_move(game, move_result, turn) do
