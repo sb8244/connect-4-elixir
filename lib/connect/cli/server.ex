23d42754b960f32077e43165d4ff796f837e231b
Stephen Bussey <steve.bussey@salesloft.com>
2016-08-20 22:49:34 -0400

    Support multiple game choices

--- lib/connect/cli/server.ex
+++ lib/connect/cli/server.ex
@@ -1,8 +1,20 @@
 defmodule Connect.Cli.Server do
   def start do
-    {:ok, game} = Connect.Game.Server.start_link(nil, %{rows: 6, columns: 7, win_size: 4})
-    IO.puts "Starting Connect 4!"
-    play_game(game, 0)
+    game_choice = IO.gets "Do you want to play Connect 4 (1) or Tic Tac Toe (2)\n"
+    case game_choice do
+      "1\n" ->
+        {:ok, game} = Connect.Game.Server.start_link(nil, %{rows: 6, columns: 7, win_size: 4})
+        IO.puts "Starting Connect 4!"
+        play_game(game, 0)
+      "2\n" ->
+        {:ok, game} = Connect.Game.Server.start_link(nil, %{rows: 3, columns: 3, win_size: 3})
+        IO.puts "Starting Tic Tac Toe!"
+        play_game(game, 0)
+      _ ->
+        IO.puts "-_-"
+        exit(:shutdown)
+    end
+
     IO.gets "Game over! Enter to start a new game....."
   end
 
